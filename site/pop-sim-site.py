96ab1bb49d84929fbcd9ca5cc44210cdfc21ed0c
Christian Harrypersad <psp1001k2006@gmail.com>
2016-04-18 19:02:07 -0400

    Added Population Simulation code on button click and some basic styling

--- site/pop-sim-site.py
+++ site/pop-sim-site.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, g, render_template
+from flask import Flask, g, render_template, request
 
 DATABASE = '../PopulationSimulator.db'
 DEBUG = True
@@ -22,11 +22,29 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+def get_pop_pred(country, year):
+    pop = int(query_db('SELECT "{col1}" FROM "{t}" WHERE "{col2}"="{val}"'.format(col1 = "Population", t = "PopulationData", col2 = "Country", val = country))[0][0])
+    mig = float(query_db('SELECT "{col1}" FROM "{t}" WHERE "{col2}"="{val}"'.format(col1 = "Migration Rate", t = "PopulationData", col2 = "Country", val = country))[0][0])
+    bir = float(query_db('SELECT "{col1}" FROM "{t}" WHERE "{col2}"="{val}"'.format(col1 = "Birth Rate", t = "PopulationData", col2 = "Country", val = country))[0][0])
+    dea = float(query_db('SELECT "{col1}" FROM "{t}" WHERE "{col2}"="{val}"'.format(col1 = "Death Rate", t = "PopulationData", col2 = "Country", val = country))[0][0])
+
+    for i in range(year):
+        pop = pop + ((mig + bir - dea) * (pop/1000))
+
+    return int(pop)
+
 @app.route('/')
 def show_all():
     query = query_db('select * from PopulationData')
-    countries = [dict(country=row[0], population=row[1]) for row in query]
+    countries = [dict(country=row[0]) for row in query]
     return render_template('population.html', countries=countries)
+
+@app.route('/calculate-years', methods=['GET', 'POST'])
+def calculate_pop():
+    years = int(request.form['years'])
+    country = request.form['countries']
+    new_pop = get_pop_pred(country, years)
+    return render_template('information.html', z=str(new_pop), years=str(years))
 
 @app.before_request
 def before_request():
